42380cba57cb57f75ab9cb7b7b8a3bed2d4d6b1f
mauve <ranger@mauve.moe>
2020-10-03 01:06:24 -0400

    Added README back

---
+++ shim.js
@@ -0,0 +1,13 @@
+global.process = require('process')
+
+// Lots of the sodium code assumes a global Buffer
+global.Buffer = require('buffer').Buffer
+
+// iOS defines a WebAssembly global, but doesn't provide a way to create instances
+// We shold delete the WebAssembly global in that case so that the tests pass
+if (typeof WebAssembly !== 'undefined' && global.WebAssembly) {
+  const canMakeInstance = global.WebAssembly.Instance || global.WebAssembly.instantiate
+  if (canMakeInstance) {
+    global.WebAssembly = undefined
+  }
+}
